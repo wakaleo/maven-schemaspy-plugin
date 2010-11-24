702044e876f5f4c75f873737d5a5150b6f457c21
John Smart <john.smart@wakaleo.com>
2010-11-24 22:47:27 +0000

    Updated to SchemaSpy 5.0

---
+++ src/test/java/com/wakaleo/schemaspy/MysqlExternalDatabaseTest.java
@@ -0,0 +1,33 @@
+package com.wakaleo.schemaspy;
+
+import com.wakaleo.schemaspy.util.DatabaseHelper;
+import java.io.File;
+import java.util.Locale;
+
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+
+/**
+ * SchemaSpyReport unit tests Test POM files is kept in test/resources/unit
+ * directory.
+ * 
+ * @author john
+ */
+public class MysqlExternalDatabaseTest extends AbstractMojoTestCase {
+
+    public MysqlExternalDatabaseTest() {
+    }
+
+    public void testMySqlConfiguration() throws Exception {
+
+        File testPom = new File(getBasedir(),
+                "src/test/resources/unit/mysql-plugin-config.xml");
+
+        SchemaSpyReport mojo = (SchemaSpyReport) lookupMojo("schemaspy", testPom);
+        mojo.executeReport(Locale.getDefault());
+
+        // check if the reports generated
+        File generatedFile = new File("./target/reports/mysql-test/schemaspy/index.html");
+        assertTrue(generatedFile.exists());
+    }
+
+}
